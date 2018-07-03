13087ba7cec74eb5f0ded330f2c5fd02ab427cbe
Luke Siecinski <luke24ski@aol.com>
2018-07-02 21:23:32 -0500

    Adding my name to the end of my h1 header with JavaScript

---
+++ index.js
@@ -0,0 +1,14 @@
+var title = document.querySelector('h1');
+
+var greetUser = function greetUser(){
+    var userName = prompt('What is your name?');
+
+    if(userName !== ''){
+        title.textContent += ' - Welcome, ' + userName;
+    }
+    else{
+        greetUser();
+    }
+};
+
+greetUser();
