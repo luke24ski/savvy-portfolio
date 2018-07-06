8ffc1ae3a96e5c64add61e7d83a086416088b671
Luke Siecinski <luke24ski@aol.com>
2018-07-05 20:30:53 -0500

    Added div #root to my HTML and js

--- index.js
+++ index.js
@@ -3,18 +3,17 @@ import Header from './src/Header';
 import Content from './src/Content';
 import Footer from './src/Footer';
 
-var greeting = document.querySelector('#greeting');
-
 var greetUser = function greetUser(){
+    var greeting = document.querySelector('#greeting');
     var firstName = prompt('What is your first name?');
     var lastName = prompt('What is your last name?');
 
     if(firstName && lastName){
         greeting.innerHTML = `
-            <div>
-                <h3>Welcome to my world,</h3>
-                <h4>${firstName} ${lastName}</h4>
-            </div>
+          <div>
+            <h3>Welcome to my home page,</h3>
+            <h4>${firstName} ${lastName}</h4>
+          </div>
         `;
     }
     else{
@@ -22,16 +21,13 @@ var greetUser = function greetUser(){
     }
 };
 
-greetUser();
-
-var initialHTML = document.body.innerHTML;
-
 document
-    .body
+    .querySelector('#root')
     .innerHTML = `
     ${Navigation}
     ${Header}
     ${Content}
     ${Footer}
-    ${initialHTML}
-    `;
+  `;
+
+greetUser();
