a8a86060563c7a8a6dff6da446297b0373c1f840
Luke Siecinski <luke24ski@aol.com>
2018-08-09 01:31:24 -0500

    editing in css

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ export default function Header(state){
             <h1 class="enblackify">${state.title}</h1>
             <img src="https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/18838814_1584720734871989_8223857747152282782_n.jpg?_nc_cat=0&oh=2f9fd6e3745a8edb54a555caa12d3c5e&oe=5BC08AC1" alt="my face">
             <div id="greeting"></div>
-            <input type="text" />
+            <input type="text" placeholder="What's your name?" />
         </div>
     </div>
     `;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,10 @@
 export default function Home(){
     return `
     <h3>
-        <p>Hello, my name is Luke Siecinski. I am a beginner web developer eager to learn and get my hands dirty in the world of web development! One main reason I am so excited to get into the field of web development & computer programming is the endless opportunity there is in the industry. Here are a few thing about me:</p>
+        <p>Hello, my name is Luke Siecinski.</p>
     </h3>
-        <ul>
-            <li>20 years old, been married for 2 years, & have a 11 month old son</li>
-            <li>Previously worked as a manager at Chick-fil-a in highschool, a truck driver for a fireplace & appliance company, & also worked in the inurance industry for two differant State Farm agents</li>
-            <li>Completed the intense Savvy Coders course and am ready to begin my journey and long-term career in the world of web development</li>
-        </ul>
+    <h4>
+        <p>I am a beginner web developer eager to learn and get my hands dirty in the world of web development! One main reason I am so excited to get into the field of web development & computer programming is the endless opportunity there is in the industry.</p>
+    </h4>
     `;
 }
