18b533f8b89b1ae11a18b7fdd8976c473ce56b2a
Luke Siecinski <luke24ski@aol.com>
2018-08-10 02:39:32 -0500

    edited more css

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,7 @@
 export default function Home(){
     return `
     <h3>
-        <p>Hello, my name is Luke Siecinski.</p>
+        <p><span class="blinking">Hello</span>, my name is Luke Siecinski.</p>
     </h3>
     <h4>
         <p>I am a beginner web developer eager to learn and get my hands dirty in the creative world of programming and web development! One main reason I am so excited to get into the field of web development is the endless opportunity there is in the industry.</p>
