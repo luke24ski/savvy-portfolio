2b27e52c4d83fa42090d098cc2c067a32c5f9ff5
Luke Siecinski <luke24ski@aol.com>
2018-08-10 02:05:22 -0500

    rephrased the content

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,7 +4,7 @@ export default function Home(){
         <p>Hello, my name is Luke Siecinski.</p>
     </h3>
     <h4>
-        <p>I am a beginner web developer eager to learn and get my hands dirty in the world of web development! One main reason I am so excited to get into the field of web development & computer programming is the endless opportunity there is in the industry.</p>
+        <p>I am a beginner web developer eager to learn and get my hands dirty in the creative world of programming and web development! One main reason I am so excited to get into the field of web development is the endless opportunity there is in the industry.</p>
     </h4>
     `;
 }
