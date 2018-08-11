dd998c8b7b5a2fef23c965cb456480160a5ff935
Luke Siecinski <luke24ski@aol.com>
2018-08-11 01:40:24 -0500

    changed background image and colors of text

--- src/Content.js
+++ src/Content.js
@@ -1,11 +1,19 @@
 import * as Pages from './Pages';
+import logo from '../assets/port_page_logo.jpg';
+
 
 export default function Content(state, posts){
     return `
-        <div id="content">
-            <div class="container">
-                ${Pages[state.body](posts)}
-            </div>
-        </div>
+    <div id="content">
+    <img src="../assets/port_page_logo.jpg">
+    <div class="container">
+    ${Pages[state.body](posts)}
+    </div>
+    </div>
     `;
 }
+
+function portLogo(){
+    return `<img scr="${logo}" />`;
+}
+portLogo();
